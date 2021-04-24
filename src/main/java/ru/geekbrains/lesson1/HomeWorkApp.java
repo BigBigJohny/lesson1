43f292a9d6f07373600508c1762d6f1ffd24beaa
agrigoriev <grigor.aleksander2011@yandex.ru>
2021-04-25 01:31:53 +0300

    'lesson1'

--- src/main/java/ru/geekbrains/lesson1/HomeWorkApp.java
+++ src/main/java/ru/geekbrains/lesson1/HomeWorkApp.java
@@ -31,6 +31,7 @@ public class HomeWorkApp {
             System.out.println("Красный");
         } else if (value > 0 && value <= 100) {
             System.out.println("Желтый");
+            // можно просто else
         } else if (value > 100) {
             System.out.println("Зеленый");
         }
